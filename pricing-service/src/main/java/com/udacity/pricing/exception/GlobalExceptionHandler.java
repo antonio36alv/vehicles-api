e7228ac8f97205544b062c880adc564e64948435
antonio36alv <ant.36alv@gmail.com>
2020-10-03 15:19:11 -0400

    handles wrong id input

--- pricing-service/src/main/java/com/udacity/pricing/exception/GlobalExceptionHandler.java
+++ pricing-service/src/main/java/com/udacity/pricing/exception/GlobalExceptionHandler.java
@@ -1,24 +1,13 @@
 package com.udacity.pricing.exception;
 
-import org.hibernate.QueryException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.data.rest.core.RepositoryConstraintViolationException;
 import org.springframework.data.rest.webmvc.RepositoryRestExceptionHandler;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.persistence.EntityNotFoundException;
-import javax.persistence.NoResultException;
-import javax.script.ScriptException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,11 +15,10 @@ import java.util.Map;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleNotFound(ResourceNotFoundException e) {
-        System.out.println("happened");
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleNotFound(IllegalArgumentException e) {
         Map<String, String> response = new HashMap<String, String>();
-        response.put("message", "Bad Request, issue with Id in query.");
+        response.put("message", "Bad Request, possible issue with id within query.");
         return new ResponseEntity<Map<String, String>>(response, HttpStatus.BAD_REQUEST);
     }
 
